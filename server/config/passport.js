aecc4e1bb94eef63a03c247a8658281923b6e70b
Max Kahn <kahn.max@gmail.com>
2016-01-09 17:28:45 -0800

    Undoing authentication for testing purposes.

--- server/config/passport.js
+++ server/config/passport.js
@@ -3,17 +3,19 @@ var constants = require('./../../constants');
 var FacebookStrategy = require('passport-facebook').Strategy;
 var usersController = require('./../components/users/usersController');
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
+// passport.serializeUser(function(user, done) {
+//   done(null, user);
+// });
 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
+// passport.deserializeUser(function(user, done) {
+//   done(null, user);
+// });
 
 passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_APP_SECRET,
+  //REMOVE THESE KEYS BEFORE PUSHING
+  //should be process.env.FACEBOOK_APP_ID and process.env.FACEBOOK_APP_SECRET
+    clientID: 216260968711459,
+    clientSecret: '48f4bf845f8fc37abf9faceb51d894cd',
     callbackURL: constants.API_URL + "/api/auth/facebook/callback",
     enableProof: true,
     profileFields: ['id', 'name','picture.type(large)', 'emails', 'displayName', 'about', 'gender']
